4e58e351901bd8b624a23fb7108f01b17af9d690
Andres Babativa <carlos3k11@gmail.com>
2024-02-16 10:09:17 -0500

    feat: adjust components to be exported publicly

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export { Breadcrumbs } from "./Breadcrumbs";
-export { BreadcrumbEllipsis } from "./Breadcrumbs/BreadcrumbEllipsis";
 export { BreadcrumbLink } from "./Breadcrumbs/BreadcrumbLink";
-export { BreadcrumbMenu } from "./Breadcrumbs/BreadcrumbMenu";
-export { BreadcrumbMenuLink } from "./Breadcrumbs/BreadcrumbMenuLink";
+
 export type { IBreadcrumbLinkProps } from "./Breadcrumbs/BreadcrumbLink";
