0310ceb4b7753a35fcbe8090ec255c6f3805ea0b
cmarin001 <cmarin6051@gmail.com>
2024-03-12 15:17:56 -0500

    fix(breadcrumbs): remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export { Breadcrumbs } from "./Breadcrumbs";
 export { BreadcrumbLink } from "./Breadcrumbs/BreadcrumbLink";
-
-export type { IBreadcrumbLinkProps } from "./Breadcrumbs/BreadcrumbLink";
